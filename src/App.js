1f9abac48385068c99f5af0e524c795060cc2cee
shivam-singh-negi <shivamsingh.comp1999@gmail.com>
2024-06-04 17:24:23 +0530

    fixes to the css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import Navbar from "./components/Navbar.jsx"
-import Hero from "./sections/Tabs/Home/Hero.jsx"
 
 
 export default function App() {
   return <>
+  
   <Navbar/>
-  <Hero/>
 
   
 
