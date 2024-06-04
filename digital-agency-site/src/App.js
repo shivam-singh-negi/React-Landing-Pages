b7734b01866b7b5c7903e854f1242a9365e5f743
shivam-singh-negi <shivamsingh.comp1999@gmail.com>
2024-06-04 12:38:53 +0530

    "adding button"

--- digital-agency-site/src/App.js
+++ digital-agency-site/src/App.js
@@ -1,8 +1,10 @@
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world this is shivam singh negi</h1>
+      <Button variant="contained">Hello world</Button>;
     </div>
   );
 }
