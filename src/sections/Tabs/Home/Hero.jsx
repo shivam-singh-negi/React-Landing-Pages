d9f86201947142272761899d23c09bcc99ae68af
shivam-singh-negi <shivamsingh.comp1999@gmail.com>
2024-06-04 16:56:53 +0530

    nav bar

---
+++ src/sections/Tabs/Home/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <>
+<section>
+tis hjk
+</section>
+<section>
+
+</section>
+        </>
+       
+        
+        
+    );
+}
+
+export default Hero;
